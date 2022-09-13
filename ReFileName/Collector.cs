3d4639436f1771ff0b0c10c7b6583b15ef937b4a
lqs1848 <253612442@qq.com>
2022-09-13 17:28:50 +0800

    init

---
+++ ReFileName/Collector.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AvCoverDownloader
+{
+    class Collector
+    {
+        public EventHandler<String> CollectorLog;
+        public EventHandler<String> ErrorLog;
+        SynchronizationContext _syncContext;
+
+        private int errName = 0;
+
+        private string _path;
+        
+        public Collector(SynchronizationContext formContext, string path)
+        {
+            _path = path;
+            _syncContext = formContext;
+        }
+
+        public void Start()
+        {
+            //client = new HttpClient();
+            new Thread(Collect).Start();
+        }
+
+        public void Collect()
+        {
+            _syncContext.Post(OutLog, "开始");
+            Director(_path);
+            _syncContext.Post(OutLog, "结束");
+            _syncContext.Post(OutLog, "修改数量:" + errName);
+        }//method 
+
+
+        void Director(string dir)
+        {
+            DirectoryInfo fdir = new DirectoryInfo(dir);
+            FileSystemInfo[] fsinfos = null;
+            try
+            {
+                fsinfos = fdir.GetFileSystemInfos();
+            }
+            catch 
+            {   
+                try
+                {
+                    fdir = new DirectoryInfo("\\\\?\\" + dir);
+                    fsinfos = fdir.GetFileSystemInfos();
+                }
+                catch {
+                    _syncContext.Post(OutLog, "Error(" + dir + ")");
+                    return;
+                }
+            }
+            
+
+
+            foreach (FileSystemInfo fsinfo in fsinfos)
+            {
+                if ((fsinfo.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden && (fsinfo.Attributes & FileAttributes.System) == FileAttributes.System)
+                {
+                    //_syncContext.Post(OutLog, "跳过" + fsinfo.FullName);
+                    continue;
+                }
+
+                if (fsinfo is DirectoryInfo)     //判断是否为文件夹
+                    Director(fsinfo.FullName);   //递归调用
+                
+                rename(fsinfo);
+            }//foreach
+
+
+        }//method
+
+
+
+        public void rename(FileSystemInfo fsinfo) {
+            string oldName = fsinfo is DirectoryInfo ? fsinfo.Name : Path.GetFileNameWithoutExtension(fsinfo.FullName);
+
+            //去除不可见字符
+            string newName = Regex.Replace(oldName, "\\p{C}", "");
+            //去除所有 Emoji
+            newName = Regex.Replace(newName, @"(\ud83c[\udf00-\udfff])|(\ud83d[\udc00-\ude4f\ude80-\udeff])|[\u2600-\u2B55]", "");
+            newName = newName.Replace("\\", "＼");
+            newName = newName.Replace("/", "／");
+            newName = newName.Replace(":", "：");
+            newName = newName.Replace("*", "＊");
+            newName = newName.Replace("!", "！");
+            newName = newName.Replace("?", "？");
+            newName = newName.Replace("<", "〈");
+            newName = newName.Replace(">", "〉");
+            newName = newName.Replace("+", "＋");
+            newName = newName.Replace("-", "－");
+            newName = newName.Replace("#", "＃");
+            newName = newName.Replace("$", "￥");
+            newName = newName.Replace("%", "％");
+            newName = newName.Replace("&", "＆");
+            newName = newName.Replace("\"", "＂");
+            newName = newName.Replace("'", "＇");
+            newName = newName.Replace("(", "（");
+            newName = newName.Replace(")", "）");
+            newName = newName.Replace(".", "．");
+            newName = Regex.Replace(newName, @"\s", " ");
+            //去除名称开头结尾的空格
+            //结尾有空格会触发 文件系统 BUG
+            newName = newName.Trim();
+
+            if (!newName.Equals(oldName))
+            {
+
+                try
+                {
+                    if (fsinfo is DirectoryInfo)
+                        Directory.Move(fsinfo.FullName, Path.GetDirectoryName(fsinfo.FullName) + "\\" + newName);
+                    else
+                        File.Move(fsinfo.FullName, Path.GetDirectoryName(fsinfo.FullName) + "\\" + newName + Path.GetExtension(fsinfo.FullName));
+                    _syncContext.Post(OutError, "(" + oldName + ") -> (" + newName + ")");
+                }
+                catch {
+                    try
+                    {
+                        if (fsinfo is DirectoryInfo)
+                            Directory.Move("\\\\?\\" + fsinfo.FullName, "\\\\?\\" + Path.GetDirectoryName(fsinfo.FullName) + "\\" + newName);
+                        else
+                            File.Move("\\\\?\\" + fsinfo.FullName, "\\\\?\\" + Path.GetDirectoryName(fsinfo.FullName) + "\\" + newName + Path.GetExtension(fsinfo.FullName));
+                    }
+                    catch 
+                    {
+                        _syncContext.Post(OutLog, fsinfo.FullName + " ERROR(" + oldName + ") -> (" + newName + ")");
+                    }
+                }
+                
+                errName++;
+            }
+            
+            
+        }// method
+
+
+        private void Output(object sendProcess, DataReceivedEventArgs output)
+        {
+            if (!String.IsNullOrEmpty(output.Data))
+            {
+                //处理方法...
+                Console.WriteLine(output.Data);
+            }
+        }
+
+        private void OutLog(object state)
+        {
+            CollectorLog?.Invoke(this, state.ToString());
+        }
+
+        private void OutError(object state)
+        {
+            ErrorLog?.Invoke(this, state.ToString());
+        }
+
+    }//class
+}
---
+++ ReFileName/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AvCoverDownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dilog.Description = "请选择识别路径";
+            DialogResult res = dilog.ShowDialog();
+            if (res == DialogResult.OK || res == DialogResult.Yes)
+            {
+                textBox1.Text = dilog.SelectedPath;
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(textBox1.Text))
+            {
+                MessageBox.Show("请先选择路径");
+                return;
+            }
+            textBox2.AppendText("开始校验\r\n");
+            Collector c = new Collector(SynchronizationContext.Current, textBox1.Text);
+            c.CollectorLog += (o, text) =>
+            {
+                textBox2.AppendText(text + "\r\n");
+            };
+            c.ErrorLog += (o, text) =>
+            {
+                textBox3.AppendText(text + "\r\n");
+            };
+            c.Start();
+        }//method
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //处理未捕获的异常   
+            //Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            //处理UI线程异常   
+            Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
+            //处理非UI线程异常   
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+        }
+
+
+        /// <summary>
+        ///这就是我们要在发生未处理异常时处理的方法，我这是写出错详细信息到文本，如出错后弹出一个漂亮的出错提示窗体，给大家做个参考
+        ///做法很多，可以是把出错详细信息记录到文本、数据库，发送出错邮件到作者信箱或出错后重新初始化等等
+        ///这就是仁者见仁智者见智，大家自己做了。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+
+            string str = "";
+            string strDateInfo = "出现应用程序未处理的异常：" + DateTime.Now.ToString() + "\r\n";
+            Exception error = e.Exception as Exception;
+            if (error != null)
+            {
+                str = string.Format(strDateInfo + "异常类型：{0}\r\n异常消息：{1}\r\n异常信息：{2}\r\n",
+                     error.GetType().Name, error.Message, error.StackTrace);
+            }
+            else
+            {
+                str = string.Format("应用程序线程错误:{0}", e);
+            }
+
+            writeLog(str);
+            MessageBox.Show("软件发送错误，请及重启软件！", "系统错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            string str = "";
+            Exception error = e.ExceptionObject as Exception;
+            string strDateInfo = "出现应用程序未处理的异常：" + DateTime.Now.ToString() + "\r\n";
+            if (error != null)
+            {
+                str = string.Format(strDateInfo + "Application UnhandledException:{0};\n\r堆栈信息:{1}", error.Message, error.StackTrace);
+            }
+            else
+            {
+                str = string.Format("Application UnhandledError:{0}", e);
+            }
+
+            writeLog(str);
+            MessageBox.Show("软件发送错误，请及重启软件！", "系统错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        /// <summary>
+        /// 写文件
+        /// </summary>
+        /// <param name="str"></param>
+        static void writeLog(string str)
+        {
+            using (StreamWriter sw = new StreamWriter(@"ErrLog.txt", true))
+            {
+                sw.WriteLine(str);
+                sw.WriteLine("---------------------------------------------------------");
+                sw.Close();
+            }
+        }
+
+    }
+}
